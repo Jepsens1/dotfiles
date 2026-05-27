7d9d47e47d7c23e22368864be224192270c0917d
Phillip Jepsen <jepsens123@outlook.dk>
2026-05-27 21:45:31 +0200

    Add .zshrc alacritty nvim

---
+++ .config/nvim/lua/plugins/osaka_jade.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "ribru17/bamboo.nvim",
+    priority = 1000,
+    opts = {
+      transparent = true,
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "bamboo",
+    },
+  },
+}
