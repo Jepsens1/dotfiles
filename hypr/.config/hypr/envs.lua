a5a21142d1e5a9c33afbabcc0adbd37e4b1db4bc
Phillip Jepsen <jepsens123@outlook.dk>
2026-06-22 15:06:16 +0200

    Add nvidia env and XDG env

--- hypr/.config/hypr/envs.lua
+++ hypr/.config/hypr/envs.lua
@@ -8,3 +8,8 @@ hl.env("XCURSOR_SIZE", "24")
 hl.env("HYPRCURSOR_SIZE", "24")
 hl.env("LIBVA_DRIVER_NAME", "nvidia")
 hl.env("__GLX_VENDOR_LIBRARY_NAME", "nvidia")
+hl.env("GBM_BACKEND", "nvidia-drm")
+
+hl.env("XDG_CURRENT_DESKTOP", "Hyprland")
+hl.env("XDG_SESSION_TYPE", "wayland")
+hl.env("XDG_SESSION_DESKTOP", "Hyprland")
